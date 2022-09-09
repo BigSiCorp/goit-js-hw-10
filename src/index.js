cd0c084f31c69a7098bcc1c34dfe58d7e15397f5
BigSiCorp <si.kocorp@gmail.com>
2022-09-09 21:47:21 +0300

    HW 10 Draft

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 3000;
+
+const input = document.querySelector('#search-box');
+
+input.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+
+function fetchCountries(event) {
+  const countryName = event.target.value.trim();
+  if (!countryName) {
+    return;
+  }
+  console.log(countryName);
+  const baseUrl = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
+  fetch(baseUrl)
+    .then(response => {
+      if (!response.ok) {
+        Notify.failure('Oops, there is no country with that name');
+        throw new Error();
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
